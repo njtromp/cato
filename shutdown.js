d261af5ed0875fe69f578a2f8a859ca0c0ff74f4
Nico Tromp <nico.tromp@strawberries.nl>
2015-03-14 10:09:42 +0100

    Added jitter detection.

--- shutdown.js
+++ shutdown.js
@@ -1,5 +1,9 @@
 /**
  * This module will shutdown the Raspberry PI when a button is pressed.
+ * In order to prevent unwanted shutdowns the button needs to be pressed
+ * twice before the shutting down is peerformed. As an extra safeguard
+ * the second press must be between half a second after and two seconds
+ * after the first press.
  */
 var execSync = require('child_process').execSync;
 
@@ -8,10 +12,30 @@ module.exports = {
 		console.log('Installing shutdown listener.');
 		var Gpio = require('onoff').Gpio,
   			button = new Gpio(4, 'in', 'rising');
-
   			button.watch(function(err, value) {
-    		shutdown();
-		});
+  				if (value == 1 && secondPressWithin2Seconds()) {
+    				shutdown();
+    			}
+			});
+	}
+
+}
+
+var lastPress = null;
+function secondPressWithin2Seconds() {
+	var now = new Date();
+	if (lastPress == null) {
+		// First press
+		lastPress = now;
+	} else if (now - lastPress < 2000) {
+		// Within two seconds from first press
+		if (now - lastPress > 500) {
+			// After half a second of first press
+			shutdown();
+		}
+	} else {
+		// Restart timing
+		lastPress = now;
 	}
 }
 
