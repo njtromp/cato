3f37eae208c0c5462089db572c7b306b215e79a4
Nico Tromp <nico.tromp@strawberries.nl>
2015-03-30 00:13:03 +0200

    Swichted to nanosecond measuring.

--- rpm-sensor.js
+++ rpm-sensor.js
@@ -1,24 +1,24 @@
 /**
- * This module will shutdown the Raspberry PI when a button is pressed.
- * In order to prevent unwanted shutdowns the button needs to be pressed
- * twice before the shutting down is peerformed. As an extra safeguard
- * the second press must be between half a second after and two seconds
- * after the first press.
+ * This module measures the RPM of the motor that eventually drives
+ * the old mechanical log.
  */
 module.exports = {
 	enableSensor: function() {
+		function triggerTime(nanotimestructure) {
+			return (nanotimestructure[0] * 1000000000) + nanotimestructure[1];
+		} 
 		console.log('Installing sensor listener.');
 		var lastSwitch = null;
 		var Gpio = require('onoff').Gpio,
   			button = new Gpio(22, 'in', 'both');
   			button.watch(function(err, value) {
   				if (value == 0) {
-  					var currentSwitch = new Date();
+  					var currentSwitch = process.hrtime();
   					if (lastSwitch == null) {
   						lastSwitch = currentSwitch;
   					} else {
-  						var delta = (currentSwitch - lastSwitch)
-  						console.log("Delta = " + delta);
+  						var deltaInNanos = (triggerTime(currentSwitch) - triggerTime(lastSwitch));
+  						console.log("RPS = " + (1000000000 / (deltaInNanos / 6)));
   						lastSwitch = currentSwitch;
   					}
   				}
