fa63b346ceeeb483272168a7e129b97b8bbf76b9
Nico Tromp <nico.tromp@gmail.com>
2015-04-08 12:33:42 +0200

    Restructed the calibration configuration.

--- calibrate.js
+++ calibrate.js
@@ -6,17 +6,12 @@
 
 "use strict";
 
-var Factory = require('./factory');
-var RPMController = Factory.createRPMController();
+//var Factory = require('./factory');
+//var RPMController = Factory.createRPMController();
 var fs = require('fs');
 
 var calibration = require('./calibration.json');
 var currentSpeed = 0;
-var knownSpeeds = null;
-
-function readCalibrationFile() {
-	knownSpeeds = Object.keys(calibration);
-}
 
 function keypressModule() {
 	var keypress = require('keypress');
@@ -26,7 +21,7 @@ function keypressModule() {
 
 	// listen for the "keypress" event
 	process.stdin.on('keypress', function (ch, key) {
-		var delta = key.ctrl ? 10 : 1;
+		var rpmChange = key.ctrl ? 10 : 1;
 	  	switch (key.name) {
 	  	case 'c':
 		  	if (key && key.ctrl) {
@@ -34,25 +29,25 @@ function keypressModule() {
 	  		}
 	  		break;
 	  	case 'up':
-	  		calibration[knownSpeeds[currentSpeed]] += delta;
+	  		calibration.speeds[currentSpeed].rpm += rpmChange;
 	  		adjustRPM();
 	  		break;
 	  	case 'down':
-	  		calibration[knownSpeeds[currentSpeed]] -= delta;
+	  		calibration.speeds[currentSpeed].rpm -= rpmChange;
 	  		adjustRPM();
 	  		break;
 	  	case 'left':
-	  		if (currentSpeed > 0) {
-	  			currentSpeed -= 1
-	  		}
-	  		showSpeedChange();
+			if (currentSpeed > 0) {
+				currentSpeed -= 1;
+			}
+			showSpeedChange();
 	  		adjustRPM();
 	  		break;
 	  	case 'right':
-	  		if (currentSpeed < (knownSpeeds.length - 1)) {
-	  			currentSpeed += 1
-	  		}
-	  		showSpeedChange();
+			if (currentSpeed < (calibration.speeds.length - 1)) {
+				currentSpeed += 1;
+			}
+			showSpeedChange();
 	  		adjustRPM();
 	  		break;
 	  	case 's':
@@ -68,17 +63,17 @@ function keypressModule() {
 }
 
 function showSpeedChange() {
-	console.log('Now calibrating ' + knownSpeeds[currentSpeed] + ' knots.');
+	console.log('Now calibrating ' + calibration.speeds[currentSpeed].speed + ' knots.');
 }
 
 function adjustRPM() {
-	// console.log(calibration[knownSpeeds[currentSpeed]]);
-	rpmController.setRPM(calibration[knownSpeeds[currentSpeed]]);
+	console.log(calibration.speeds[currentSpeed].rpm);
+	// rpmController.setRPM(calibration.speeds[currentSpeed].rpm);
 }
 
 function saveCalibrationFile() {
 	console.log('Saving calibration table');
-	fs.writeFile("./calibration.json", JSON.stringify(calibration), function(err) {
+	fs.writeFile("./calibration.json", JSON.stringify(calibration, null, '\t'), function(err) {
     	if(err) {
         	return console.log(err);
     	}
@@ -87,6 +82,5 @@ function saveCalibrationFile() {
 	}); 
 }
 
-readCalibrationFile();
 keypressModule();
 showSpeedChange();
