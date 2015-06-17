429ff5eca03765c351d45d24e5fcae0aa29e9700
Nico Tromp <nico.tromp@gmail.com>
2015-06-17 11:43:10 +0200

    Started development for a Node server that will provide a responsive HTML page for speed, direction etc.

---
+++ server/server.js
@@ -0,0 +1,33 @@
+/**
+ * Skeleton for pushing speed updates to browsers.
+ */
+
+"use strict";
+
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8080);
+
+function handler (request, response) {
+    // Only serve files if they exist
+    console.log(request.url);
+    fs.exists(__dirname + request.url, function() {
+        fs.readFile(__dirname + request.url,
+        function (err, data) {
+            if (err) {
+                response.writeHead(500);
+                return response.end('Error loading ' + request.url);
+              }
+
+            response.writeHead(200);
+            response.end(data);
+        });
+    });
+}
+
+// Mimic varying speed
+setInterval(function() {
+    io.emit('speed', {knots: Math.round(Math.random()*6.0*10) / 10.0 });
+}, 1000);
